4660c85f60981755b2529de4403a24cd8dbd78e3
lykariii <karendecleva3@gmail.com>
2023-09-11 15:12:35 -0400

    db dnv

--- db.js
+++ db.js
@@ -36,3 +36,21 @@ async function addData() {
     store.add({ name: 'Fulano' }); 
     await tx.done;
 }
+
+async function getData() {
+    if (db == undefined) {
+    showResult("O banco de dados está fechado"); return;
+    }
+    const tx = await db.transaction('pessoas', 'readonly') 
+    const store = tx.objectStore('pessoas'); 
+    const value = await store.getAll(); 
+    if (value) {
+    showResult("Dados do banco: " + JSON.stringify(value))
+    } else {
+    showResult("Não há nenhum dado no banco!")
+    }
+}
+
+function showResult(text){
+    document.querySelector("output").innerHTML = text;
+}
